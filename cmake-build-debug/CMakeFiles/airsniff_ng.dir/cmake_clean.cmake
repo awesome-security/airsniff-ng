78e3e0ea1fa7f3093cc4161f60bafdc491213dcc
deltaxflux <deltax.fluxion@gmail.com>
2017-11-16 19:27:54 +0100

    Fix bad function

---
+++ cmake-build-debug/CMakeFiles/airsniff_ng.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/airsniff_ng.dir/src/help.cpp.o"
+  "CMakeFiles/airsniff_ng.dir/src/main.cpp.o"
+  "airsniff_ng.pdb"
+  "airsniff_ng"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/airsniff_ng.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
